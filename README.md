eff86e51faf26f4b02ba784053cd8e6d29009e44
leping.wlp <leping.wlp@alibaba-inc.com>
2021-07-16 15:27:39 +0800

    feat: done

---
+++ README.md
@@ -0,0 +1,15 @@
+# 摸鱼之自由窗口
+
+![](https://img.alicdn.com/imgextra/i2/O1CN01dcUq9f1fbUrGgKtMl_!!6000000004025-2-tps-128-128.png)
+
+沉浸式摸鱼。
+
+## 使用
+
+```
+cmd + shift + p
+
+> freewindow
+```
+
+![](https://img.alicdn.com/imgextra/i3/O1CN01f8EiDc26BqIYmHrLg_!!6000000007624-2-tps-1312-736.png)
---
+++ src/extension.ts
@@ -0,0 +1,27 @@
+import * as vscode from "vscode";
+const path = require("path");
+const fs = require("fs");
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand("freewindow.webview", () => {
+    const html = fs.readFileSync(
+      path.join(context.extensionPath, "./src/freeWindow.html"),
+      "utf-8"
+    );
+
+    const panel = vscode.window.createWebviewPanel(
+      "freewindow.webview",
+      "Free-Window",
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+        retainContextWhenHidden: true,
+      }
+    );
+    panel.webview.html = html;
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
