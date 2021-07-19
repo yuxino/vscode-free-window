1d17c98da6e0e4c9939c61aa9b15ebc0f31d8878
leping.wlp <leping.wlp@alibaba-inc.com>
2021-07-19 21:38:32 +0800

    feat: 两列

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
     const panel = vscode.window.createWebviewPanel(
       "freewindow.webview",
       "Free-Window",
-      vscode.ViewColumn.One,
+      vscode.ViewColumn.Two,
       {
         enableScripts: true,
         retainContextWhenHidden: true,
